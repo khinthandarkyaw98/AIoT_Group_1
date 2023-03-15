1cd7b038a589812cdbac2ee04de55d5232ffc86f
Khin Thandar Kyaw <50408474+khinthandarkyaw98@users.noreply.github.com>
2023-03-15 20:23:32 +0700

    Delete Webapp/Software_Design_Project

--- webapp/software_design_project/router.py
+++
@@ -1,9 +0,0 @@
-from software_design.viewsets import StudentViewset
-from rest_framework import routers
-
-router = routers.DefaultRouter()
-router.register('student', StudentViewset)
-
-# localhost:p/api/student/5
-# GET, POST, UPDATE, DELETE
-# list, retrieve
