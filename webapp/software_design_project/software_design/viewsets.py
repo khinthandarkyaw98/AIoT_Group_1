afc64c3a1f504adbd73a2090bbd64772a739db20
Lian Lian <51283671+hrangkap@users.noreply.github.com>
2023-03-17 16:12:15 +0700

    Update viewset.py

--- webapp/software_design_project/software_design/viewsets.py
+++ webapp/software_design_project/software_design/viewsets.py
@@ -6,4 +6,9 @@ class StudentViewset(viewsets.ModelViewSet):
     queryset = models.Student.objects.all()
     serializer_class = serializers.StudentSerializer
 
+class StaffViewset(viewsets.ModelViewSet):
+    queryset = models.Staff.objects.all()
+    serializer_class = serializers.StaffSerializer
+
+
 # list(), retrieve(), create(), update(), destroy()
