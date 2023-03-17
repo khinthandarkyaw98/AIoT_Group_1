16d5977087ce29487e978badf1e481e4e01cf857
Lian Lian <51283671+hrangkap@users.noreply.github.com>
2023-03-17 16:09:39 +0700

    Update model.py

--- webapp/software_design_project/software_design/models.py
+++ webapp/software_design_project/software_design/models.py
@@ -1,12 +1,37 @@
-from django.db import models
+from django.shortcuts import render,HttpResponse,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from rest_framework.parsers import JSONParser
+from django.http.response import JsonResponse
 
-# Create your models here.
+from . models import Student
+from . serializers import StudentSerializer
 
-class Student(models.Model):
-    firstname = models.CharField(max_length=50)
-    lastname = models.CharField(max_length=50)
-    
-# Create    /   Insert  /   Add - POST
-# Retrieve  /   Fetch - GET
-# Update    / Edit - PUT
-# Delete    / Remove - DELETE
+def loginPage(request):
+    if request.method=='POST':
+        username=request.POST.get('username')
+        pass1=request.POST.get('password')
+        user=authenticate(request,username=username,password=pass1)
+        if user is not None:
+            login(request,user)
+            return redirect('home')
+        else:
+            return HttpResponse ("Username or Password is incorrect!!!")
+            print 
+
+    return render (request,'login.html')
+
+def homePage(request):
+    return render(request, 'home.html')
+
+def logoutPage(request):
+    return redner(request, 'logout.html')
+
+def studentApi(request):
+    if request.method =='GET':
+        student = Student.objects.all()
+        student_serializer = StudentSerializer(student, many=True)
+        return JsonResponse(student_serializer.data, safe=False)
+
+# Create your views here.
