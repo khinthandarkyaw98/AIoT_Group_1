b2a409bd356066adc101c012bed7e37026435028
Lian Lian <51283671+hrangkap@users.noreply.github.com>
2023-03-17 16:13:38 +0700

    Update router.py

--- webapp/software_design_project/software_design_project/router.py
+++ webapp/software_design_project/software_design_project/router.py
@@ -1,8 +1,10 @@
 from software_design.viewsets import StudentViewset
+from software_design.viewsets import StaffViewset
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('student', StudentViewset)
+router.register('staff', StaffViewset)
 
 # localhost:p/api/student/5
 # GET, POST, UPDATE, DELETE
