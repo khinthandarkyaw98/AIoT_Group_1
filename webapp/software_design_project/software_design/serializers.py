cd602a06d677afec6311612f0577827d6094378c
Lian Lian <51283671+hrangkap@users.noreply.github.com>
2023-03-17 16:10:16 +0700

    Update serializer.py

--- webapp/software_design_project/software_design/serializers.py
+++ webapp/software_design_project/software_design/serializers.py
@@ -2,8 +2,16 @@
 
 from rest_framework import serializers
 from .models import Student
+from .models import Staff
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ('__all__')
+
+class StaffSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Staff
+        fields = ('__all__')
+    
+   
