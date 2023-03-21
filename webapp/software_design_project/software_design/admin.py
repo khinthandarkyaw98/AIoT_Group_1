ed1918b77b47ec96e44e863e30a6b783063d74bb
Lian Lian <51283671+hrangkap@users.noreply.github.com>
2023-03-21 13:38:57 +0700

    Update admin.py

--- webapp/software_design_project/software_design/admin.py
+++ webapp/software_design_project/software_design/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Student
 
-# Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('student_id', 'student_name', 'student_point', 'student_passport')
+
+admin.site.register(Student, PersonAdmin)
