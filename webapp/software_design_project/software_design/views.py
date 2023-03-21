9c685099e32fa2d19c16c341fc43f19bb7b68f6e
Lian Lian <51283671+hrangkap@users.noreply.github.com>
2023-03-21 13:39:55 +0700

    Update views.py

--- webapp/software_design_project/software_design/views.py
+++ webapp/software_design_project/software_design/views.py
@@ -1,46 +1,43 @@
 from django.shortcuts import render,HttpResponse,redirect
-from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
 from django.contrib.auth.models import User
-from django.template import loader
-from django.http import HttpResponse
-
-
+from django.conf import settings
+from django.contrib.auth import authenticate, login, logout
 from . models import Student
 from . serializers import StudentSerializer
+from django.contrib.auth import authenticate, login
 
 def loginPage(request):
-    if request.method=='POST':
-        stu_id=request.POST.get('student_id')
-        pass1=request.POST.get('password')
-       # user=authenticate(request,student_id=stu,student_passport=pass1)
-
-        students = Student.objects.all().values()
-        context = {'students': students}
-        
+    if request.method == 'POST':
+        stu_id = request.POST.get('student_id')
+        pass1 = request.POST.get('password')
+        student = Student.objects.filter(student_id = stu_id)
         for stu in student:
-            if stu['student_id'] == stu_id and stu['student_passport'] == pass1:
-                return render(request, 'home.html', context)
+            if stu.student_passport == pass1:
+                context = {'students': student}
+                print('Login Success')
+                return render(request,'home.html',context)
             else:
-                print('Failed')
-    return render (request,'home.html')
+                print('Login Failed')
+                return HttpResponse('Incorrect student id or password')
+    return render(request, 'login.html')
 
+@login_required(login_url=settings.LOGIN_URL)
 def homePage(request):
-    students = Student.objects.all()
+    student_id = request.student.student_id
+    students = Student.objects.filter(student_id=student_id)
     context = {'students': students}
     return render(request, 'home.html', context)
-   
 
 def logoutPage(request):
-    return redner(request, 'logout.html')
+    return render(request, 'logout.html')
 
 def studentApi(request):
     if request.method =='GET':
         student = Student.objects.all()
         student_serializer = StudentSerializer(student, many=True)
         return JsonResponse(student_serializer.data, safe=False)
-  
 
 # Create your views here.
