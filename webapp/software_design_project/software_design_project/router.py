687574f967e0dd19ea75ddb74d27fcb248966e68
Khin Thandar Kyaw <50408474+khinthandarkyaw98@users.noreply.github.com>
2023-04-06 21:32:34 +0700

    fixed router.py

--- webapp/software_design_project/software_design_project/router.py
+++ webapp/software_design_project/software_design_project/router.py
@@ -1,11 +1,12 @@
 from software_design.viewsets import StudentViewset
-from software_design.viewsets import StaffViewset
+from software_design.viewsets import StaffViewset, Mac_pointViewset
 from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register('student', StudentViewset)
 router.register('staff', StaffViewset)
+router.register('mac_point', Mac_pointViewset)
 
 # localhost:p/api/student/5
 # GET, POST, UPDATE, DELETE
-# list, retrieve
+# list, re
