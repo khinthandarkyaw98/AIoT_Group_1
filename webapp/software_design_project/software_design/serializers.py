286345fdd4d509155e98203e4125f2d1730f7bfa
Aeint Shune Thar <126539280+alicesthar@users.noreply.github.com>
2023-04-06 21:25:39 +0700

    Update serializers.py

--- webapp/software_design_project/software_design/serializers.py
+++ webapp/software_design_project/software_design/serializers.py
@@ -1,7 +1,7 @@
 # api <-> mobile app/ web app / etc. json
 
 from rest_framework import serializers
-from .models import Student
+from .models import Student, Mac_point
 from .models import Staff
 
 class StudentSerializer(serializers.ModelSerializer):
@@ -9,9 +9,12 @@ class StudentSerializer(serializers.ModelSerializer):
         model = Student
         fields = ('__all__')
 
+class Mac_point(serializers.ModelSerializer):
+    class Meta:
+        model = Mac_point
+        fields = ('__all__')
+
 class StaffSerializer(serializers.ModelSerializer):
     class Meta:
         model = Staff
         fields = ('__all__')
-    
-   
