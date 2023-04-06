f028e04b600b7a2cd3691441e5ade36f4555d403
Khin Thandar Kyaw <50408474+khinthandarkyaw98@users.noreply.github.com>
2023-04-06 21:30:17 +0700

    fixed viewsets.py

--- webapp/software_design_project/software_design/viewsets.py
+++ webapp/software_design_project/software_design/viewsets.py
@@ -1,14 +1,21 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, status
 from . import models
 from . import serializers
 
 class StudentViewset(viewsets.ModelViewSet):
     queryset = models.Student.objects.all()
     serializer_class = serializers.StudentSerializer
+    permission_classes = [permissions.AllowAny]
+
+class Mac_pointViewset(viewsets.ModelViewSet):
+    queryset = models.Mac_point.objects.all()
+    serializer_class = serializers.Mac_point
+    permission_classes = [permissions.AllowAny]
 
 class StaffViewset(viewsets.ModelViewSet):
     queryset = models.Staff.objects.all()
     serializer_class = serializers.StaffSerializer
+    permission_classes = [permissions.AllowAny]
 
 
 # list(), retrieve(), create(), update(), destroy()
