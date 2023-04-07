a66070acccb5a3911c6dee68b9026be4cec9d747
Aeint Shune Thar <126539280+aeintshune-thar@users.noreply.github.com>
2023-04-07 15:14:37 +0700

    Update serializers.py

--- webapp/software_design_project/software_design/serializers.py
+++ webapp/software_design_project/software_design/serializers.py
@@ -4,15 +4,20 @@ from rest_framework import serializers
 from .models import Student, Mac_point
 from .models import Staff
 
+# Define serializers for the application's data models to facilitate JSON data exchange.
+# StudentSerializer for serializing/deserializing Student model data.
+
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Student
-        fields = ('__all__')
+        model = Student # Specify the model to be serialized
+        fields = ('__all__') # Specify the model to be serialized
 
+        
+# Mac_pointSerializer for serializing/deserializing Mac_point model data.
 class Mac_point(serializers.ModelSerializer):
     class Meta:
-        model = Mac_point
-        fields = ('__all__')
+        model = Mac_point # Specify the model to be serialized
+        fields = ('__all__') # Serialize all fields of the model
 
 class StaffSerializer(serializers.ModelSerializer):
     class Meta:
